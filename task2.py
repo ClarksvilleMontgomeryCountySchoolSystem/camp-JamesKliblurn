3a17197a9c7f04b93a75b56fa4c934c9a4173ab3
JamesKliblurn <james.kilburn062@students.cmcss.net>
2026-02-17 12:25:38 -0600

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,38 @@
+from data import first, last, birth, days, p_first, p_last, phone, street, city, state, zip_code
+
+# For each variable convert to the appropriate data type.
+
+
+# Not all variables will need conversion.
+
+# first
+
+# last
+
+
+# birth
+birth = int(birth)
+
+# days
+days = int(days)
+
+# p_first
+
+
+# p_last
+
+
+# phone
+
+
+# street
+
+
+# city
+
+
+# state
+
+
+# zip_code
+
